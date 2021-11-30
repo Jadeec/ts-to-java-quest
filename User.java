03668dfa5a3bc69c1f520bdad26b4ffe0bd1751a
Jade <jade.cmail@gmail.com>
2021-11-30 11:27:21 +0100

    convert all ts files to java files

---
+++ User.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class User {
+    public static void main(String[] args) {
+    Date birthdate; 
+    int size; 
+    Photo[] photos; 
+    Address address; 
+    }
+}
